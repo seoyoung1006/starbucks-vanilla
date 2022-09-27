c39b2de25dac9e90ab6526a7ed4caffbdeb8de5f
toystory <74535088+seoyoung1006@users.noreply.github.com>
2022-09-27 14:56:25 +0900

    Update sub-menu

--- js/main.js
+++ js/main.js
@@ -3,4 +3,14 @@ const searchInputEl = searchEl.querySelector('input');
 
 searchEl.addEventListener('click', function() {
   searchInputEl.focus();//focus강제 적용
-})
+});
+
+searchInputEl.addEventListener('focus', function() {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function() {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
